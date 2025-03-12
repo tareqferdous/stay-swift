b0059757fb8f34a3e01054c774093af6ef02afe8
tareqferdous <16101017@uap-bd.edu>
2025-03-12 15:50:40 +0600

    mongoose setup complete

--- components/hotel/HotelList.jsx
+++ components/hotel/HotelList.jsx
@@ -1,10 +1,13 @@
+import { getAllHotels } from "@/queries";
 import HotelCard from "./HotelCard";
 
-const HotelList = () => {
+const HotelList = async () => {
+  const hotels = await getAllHotels();
+  console.log(hotels);
   return (
     <div className="col-span-9">
       <div className="space-y-4">
-       <HotelCard />
+        <HotelCard />
       </div>
     </div>
   );
