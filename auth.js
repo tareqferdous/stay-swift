6c34fa4e16ec93f93ebf3b81007312d551a30abd
tareqferdous <16101017@uap-bd.edu>
2025-03-14 05:55:25 +0600

    creds login added

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import mongoClientPromise from "./database/mongoClientPromise";
+import { userModel } from "./models/user-model";
 
 export const {
   handlers: { GET, POST },
@@ -12,7 +14,37 @@ export const {
   adapter: MongoDBAdapter(mongoClientPromise, {
     databaseName: process.env.ENVIRONMENT,
   }),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+
+          if (user) {
+            const isMatch = user.email === credentials.email;
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Email or password mismatch");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
