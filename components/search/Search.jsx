0cbc7d884557c23fd93ba4bfe8d55872a5280319
tareqferdous <16101017@uap-bd.edu>
2025-03-16 21:30:21 +0600

    search

--- components/search/Search.jsx
+++ components/search/Search.jsx
@@ -1,4 +1,37 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
 const Search = ({ fromList }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [searchTerm, setSearchTerm] = useState({
+    destination: "",
+    checkin: "",
+    checkout: "",
+  });
+
+  const [allowSearch, setAllowSearch] = useState(true);
+
+  const handleInputs = (event) => {
+    const state = {
+      ...searchTerm,
+      [event.target.name]: event.target.value,
+    };
+    if (
+      new Date(state.checkin).getTime() > new Date(state.checkout).getTime()
+    ) {
+      setAllowSearch(false);
+    } else {
+      setAllowSearch(true);
+    }
+
+    setSearchTerm(state);
+  };
+
   return (
     <>
       <div className="lg:max-h-[250px] mt-6">
@@ -6,12 +39,16 @@ const Search = ({ fromList }) => {
           <div>
             <span>Destination</span>
             <h4 className="mt-2">
-              <select name="destination" id="destination">
-                <option value="Bali">Bali</option>
-                <option value="Bali">Coxs Bazar</option>
-                <option value="Bali">Sylhet</option>
-                <option value="Bali">Saint Martin</option>
-                <option value="Bali">Bali</option>
+              <select
+                onChange={handleInputs}
+                name="destination"
+                id="destination"
+              >
+                <option value="Puglia">Puglia</option>
+                <option value="Catania">Catania</option>
+                <option value="Palermo">Palermo</option>
+                <option value="Frejus">Frejus</option>
+                <option value="Paris">Paris</option>
               </select>
             </h4>
           </div>
@@ -19,20 +56,30 @@ const Search = ({ fromList }) => {
           <div>
             <span>Check in</span>
             <h4 className="mt-2">
-              <input type="date" name="checkin" id="checkin" />
+              <input
+                onChange={handleInputs}
+                type="date"
+                name="checkin"
+                id="checkin"
+              />
             </h4>
           </div>
 
           <div>
             <span>Checkout</span>
             <h4 className="mt-2">
-              <input type="date" name="checkout" id="checkout" />
+              <input
+                onChange={handleInputs}
+                type="date"
+                name="checkout"
+                id="checkout"
+              />
             </h4>
           </div>
         </div>
       </div>
 
-      <button className="search-btn">
+      <button disabled={!allowSearch} className="search-btn">
         🔍️ {fromList ? "Modify Search" : "Search"}
       </button>
     </>
