772c5e36725222a5660ee9d2b35c6c370d377f49
tareqferdous <16101017@uap-bd.edu>
2025-03-19 11:41:14 +0600

    added ooking

--- app/(home)/bookings/page.js
+++ app/(home)/bookings/page.js
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import ProfileInfo from "@/components/user/ProfileInfo";
 import PastBooking from "@/components/user/booking/PastBooking";
 import UpcomingBooking from "@/components/user/booking/UpcomingBooking";
+import { getBookingsByUser, getUserByEmail } from "@/database/queries";
 import { redirect } from "next/navigation";
 
 const BookingsPage = async () => {
@@ -9,6 +10,18 @@ const BookingsPage = async () => {
   if (!session) {
     redirect("/login");
   }
+
+  const loggedInUser = await getUserByEmail(session?.user?.email);
+  const bookings = await getBookingsByUser(loggedInUser?.id);
+
+  const pastBookings = bookings.filter((booking) => {
+    return new Date().getTime() > new Date(booking.checkin).getTime();
+  });
+
+  const upcomingBookings = bookings.filter((booking) => {
+    return new Date().getTime() < new Date(booking.checkin).getTime();
+  });
+
   return (
     <>
       <section className="mt-[100px]">
@@ -19,8 +32,8 @@ const BookingsPage = async () => {
       <section>
         <div className="container">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <PastBooking />
-            <UpcomingBooking />
+            <PastBooking bookings={pastBookings} />
+            <UpcomingBooking bookings={upcomingBookings} />
           </div>
         </div>
       </section>
