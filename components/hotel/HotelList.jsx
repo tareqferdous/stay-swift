13615f434297346c45c904f69292a2cb2104ffe5
tareqferdous <16101017@uap-bd.edu>
2025-03-30 22:42:34 +0600

    update hotel list

--- components/hotel/HotelList.jsx
+++ components/hotel/HotelList.jsx
@@ -1,4 +1,5 @@
 import { getAllHotels } from "@/database/queries";
+import { RiHotelLine } from "react-icons/ri";
 import HotelCard from "./HotelCard";
 
 const HotelList = async ({
@@ -18,6 +19,16 @@ const HotelList = async ({
 
   return (
     <div className="col-span-9">
+      <div className="flex items-center space-x-2 pt-0 pb-4">
+        <RiHotelLine className="text-2xl text-rose-500" />
+        <span className="font-bold text-xl">
+          {destination}:{" "}
+          <span className="font-semibold">
+            {allHotels.length}{" "}
+            {`${allHotels.length > 1 ? "Properties" : "Property"} found`}
+          </span>
+        </span>
+      </div>
       <div className="space-y-4">
         {allHotels.map((hotel) => (
           <HotelCard
--- components/search/filter/Filter.jsx
+++ components/search/filter/Filter.jsx
@@ -1,4 +1,5 @@
 import SortHotel from "@/components/sort/SortHotel";
+import { FaFilter } from "react-icons/fa";
 import FilterByAmenities from "./FilterByAmenities";
 import FilterByPriceRange from "./FilterByPriceRange";
 import FilterByStarCategory from "./FilterByStarCategory";
@@ -6,12 +7,17 @@ import FilterByStarCategory from "./FilterByStarCategory";
 const Filter = () => {
   return (
     <>
-      <div className="col-span-3 border border-gray/20 space-y-4 shadow-lg rounded-xl p-5">
-        <SortHotel />
-
-        <FilterByPriceRange />
-        <FilterByStarCategory />
-        <FilterByAmenities />
+      <div className="col-span-3 border border-gray/20 space-y-4 shadow-lg rounded-xl ">
+        <div className="flex items-center space-x-2 border-b p-4">
+          <FaFilter className="text-lg text-rose-500" />
+          <span className="font-bold text-xl">Filter</span>
+        </div>
+        <div className="pt-0 px-5 pb-5">
+          <SortHotel />
+          <FilterByPriceRange />
+          <FilterByStarCategory />
+          <FilterByAmenities />
+        </div>
       </div>
     </>
   );
